035705c182eb3921fdec676d67adf1939546bc13
LETICIA SILVA <leticiazooe@gmail.com>
2023-03-10 20:08:36 -0300

    Criação dos metodos set

--- ConsoleApp_Note/Program.cs
+++ ConsoleApp_Note/Program.cs
@@ -4,4 +4,10 @@ using ConsoleApp_Note;
 Console.WriteLine("Hello, World!");
 
 Produto p1 = new Produto(1,10.90M,"Cervejinha");
-Console.WriteLine(p1.getDescricao() + "" + p1.getId();
+Console.WriteLine(p1.getDescricao()+ ""+p1.getId());
+
+
+p1.setValor(11.85M);
+p1.setDescricao("Cerveja Skol 1L");
+
+Console.WriteLine(p1.getDescricao()+""+p1.getValor());
